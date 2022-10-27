0f682b73551675c024cefab056404e067c2163a4
Alexandr Gogichaev <gogich.83@mail.ru>
2022-10-27 18:49:37 +0300

    first commit)

---
+++ target/test-classes/test.sql
@@ -0,0 +1,24 @@
+#insert into users (name, age, email) values ('Alex', 39, 'gogich.83@mail.ru');
+#select name, age from users where id = 1;
+#select * from user;
+#update users set name='Steve', age = 33 where id = 2;
+#delete from user where id=3;
+#CREATE TABLE user
+#(id int auto_increment
+#);
+#select * from users;
+#delete from user where ` id`=2;
+#insert into user (name, lastName, age) VALUES ('Roman','Rutov', 29);
+#update user set name = 'Maria',lastname = 'Smirnova',age = 33 where id = 3;
+#select name from user where ` id`=3;
+#select name||lastname from user;
+#insert into users (name, lastName, age) VALUES ('Alexandr','Gogichaev', 39);
+#insert into users (name, lastName, age) VALUES ('Semen','Rutov', 24);
+#insert into users (name, lastName, age) VALUES ('Maria','Smirnova', 34);
+#insert into users (name, lastName, age) VALUES ('Elena','Ivanova', 45);
+#select name from users where id=3;
+
+
+
+
+
